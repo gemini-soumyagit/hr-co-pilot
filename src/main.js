741d8921aa2e5a54717802e52ec3e86ee8b6e09d
Soumyagit123 <115079729+Soumyagit123@users.noreply.github.com>
2024-10-17 13:14:16 +0530

    updated function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 
 export default async ({ req, res, log, error }) => {
-  return res.send("welcome to era of AI !")
+  if(req.metho.get){
+
+    return res.send("welcome to era of AI !")
+  }
+  if(req.metho.post){
+
+    return res.json({
+      'sendData':req.body,
+      'googleAPiKey': process.env.GOOGLE_GEMINI_API_KEY
+    })
+  }
+
 };
