f7650e0519ec202210a670ba4b3fcdad217b4e7c
Appwrite <team@appwrite.io>
2024-10-17 13:21:43 +0000

    Create 'first_function' function

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
+import { Client, Users } from 'node-appwrite';
 
+// This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
-  if(req.method == 'GET'){
+  // You can use the Appwrite SDK to interact with other services
+  // For this example, we're using the Users service
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-key'] ?? '');
+  const users = new Users(client);
 
-    return res.send("welcome to era of AI !")
+  try {
+    const response = await users.list();
+    // Log messages and errors to the Appwrite Console
+    // These logs won't be seen by your end users
+    log(`Total users: ${response.total}`);
+  } catch(err) {
+    error("Could not list users: " + err.message);
   }
-  if(req.method == 'POST'){
 
-    return res.json({
-      'sendData':req.body,
-      'sendData':"hello",
-
-      // 'googleAPiKey': process.env.GOOGLE_GEMINI_API_KEY
-    })
+  // The req object contains the request data
+  if (req.path === "/ping") {
+    // Use res object to respond with text(), json(), or binary()
+    // Don't forget to return a response!
+    return res.text("Pong");
   }
 
+  return res.json({
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
+  });
 };
